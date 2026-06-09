09782841a72c432bd1c5d5e9c69c23fcb4a436a2
Tayna <taynavicente2019@gmail.com>
2026-06-08 21:32:43 -0300

    feat: adicionando configuracoes para o backend

---
+++ back-end/src/main/java/br/com/financeiro/SistemaFinanceiroApplication.java
@@ -0,0 +1,13 @@
+package br.com.financeiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaFinanceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaFinanceiroApplication.class, args);
+	}
+
+}
